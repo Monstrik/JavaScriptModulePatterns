4a7f6df28c567a8dfbecac2ca5a37cbb6199574d
Monstrik <coolspammail@gmail.com>
2015-07-20 14:54:49 +0300

    end

---
+++ 04-Inheritance/index.js
@@ -0,0 +1,18 @@
+﻿var FileLogger = require('./file_logger');
+var log1 = FileLogger();
+
+
+log1.log('one');
+log1.info('two');
+
+
+var log2 = FileLogger(
+    {
+        path: __dirname + '\\tmp\\log2.txt',
+        defaultLevel: 'warn'
+    });
+
+log2.log('three');
+log2.critical('four');
+
+
